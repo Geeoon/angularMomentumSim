2fe2ac7beeddc768804f49fb592194c0666a5665
Geeoon Chung <geeoon13@gmail.com>
2020-04-15 14:42:03 -0500

    Added satellite manager.

--- angularMomentumSim/src/classes/SatelliteManager.cpp
+++ angularMomentumSim/src/classes/SatelliteManager.cpp
@@ -1,1 +1,15 @@
 #include "SatelliteManager.h"
+
+SatelliteManager::SatelliteManager() {
+
+}
+
+void SatelliteManager::addSatellite(double ma, double aV, double r) {
+	satellites.push_back(Satellite(ma, aV, r));
+}
+
+void SatelliteManager::update() {
+	for (size_t i = 0; i < satellites.size(); i++) {
+		satellites[i].update();
+	}
+}
--- angularMomentumSim/src/classes/SatelliteManager.h
+++ angularMomentumSim/src/classes/SatelliteManager.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <vector> 
+#include "Satellite.h"
+
 class SatelliteManager
 {
+public:
+	SatelliteManager();
+
+	void addSatellite(double ma, double aV, double r);
+
+	void update();
+private:
+	std::vector<Satellite> satellites;
 };
 
