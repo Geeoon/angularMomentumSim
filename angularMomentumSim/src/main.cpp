e0b02e02d63c001a1ba2284fe031c21b9461b042
Geeoon Chung <geeoon13@gmail.com>
2020-04-15 11:41:47 -0500

    Created new class UIManager and fixed distortion when window is resized.

--- angularMomentumSim/src/main.cpp
+++ angularMomentumSim/src/main.cpp
@@ -5,13 +5,21 @@
 int main() {
 	Satellite object(500, 2, 20);
 	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Angular Momentum Visualizer");
+
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Resized)
+			{
+				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+			}
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 		}
+
+
 
 		window.clear();
 		object.draw(window);
