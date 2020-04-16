7f91affa6546c88d0b7a9f2e74fe9919fd3e329a
Geeoon Chung <geeoon13@gmail.com>
2020-04-16 00:28:57 -0500

    Removed old commented out code and added new comments.

--- angularMomentumSim/src/main.cpp
+++ angularMomentumSim/src/main.cpp
@@ -7,37 +7,18 @@ int main() {
 	SatelliteManager satelliteMan;
 	satelliteMan.addSatellite(500, 200, 100);
 	UIManager ui;
-	
-	//sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Angular Momentum Visualizer");
-	//window.setFramerateLimit(100);
-	/*
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Resized)
-			{
-				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
-				window.setView(sf::View(visibleArea));
-			}
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-
-		satelliteMan.update();
-
-		window.clear();
-		satelliteMan.draw(window);
-		window.display();
-	}
-	*/
 
 	while (ui.isUIOpen()) {
+		//update stuff
 		ui.update();
 		satelliteMan.update();
+
+		//drawing stuff
 		ui.getWindow().clear();
+
 		ui.draw();
 		satelliteMan.draw(ui.getWindow());
+
 		ui.getWindow().display();
 	}
 
